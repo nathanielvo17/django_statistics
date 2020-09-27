ce7787fccf6bf12dbc39e872aef8e12938070ba9
Nathaniel Vo <nathanielvo17@ou.edu>
2020-09-27 06:48:31 -0500

    Added Part3

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Nathaniel Vo', 
+        'title': 'Blog Post',
+        'content': 'first post', 
+        'date_posted': '9/27/2020'
+    },
+    {
+        'author': 'Nathaniel Vo\'s Twin', 
+        'title': 'Blog Post 2',
+        'content': 'second post', 
+        'date_posted': '9/27/2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h2>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
